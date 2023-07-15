341d47ae35242058cccbde5aca5e1840608792b0
css <cauefc12@gmail.com>
2023-07-15 19:42:38 -0300

    SQL query

--- src/main/java/com/user/dslist/services/GameService.java
+++ src/main/java/com/user/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.user.dslist.dto.GameDTO;
 import com.user.dslist.dto.GameMinDTO;
 import com.user.dslist.entities.Game;
+import com.user.dslist.projections.GameMinProjection;
 import com.user.dslist.repositories.GameRepository;
 
 @Service
@@ -26,6 +27,12 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
